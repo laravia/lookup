c53f5e28e0e0911e2b562c0ff507f39035b5f312
manuel <code@leunam.art>
2023-10-16 17:52:35 +0200

    [feat] added remove method to screen

--- app/Orchid/Screens/LookupScreen.php
+++ app/Orchid/Screens/LookupScreen.php
@@ -2,10 +2,12 @@
 
 namespace Laravia\Lookup\App\Orchid\Screens;
 
+use Illuminate\Http\Request;
 use Laravia\Lookup\App\Models\Lookup as ModelsLookup;
 use Laravia\Lookup\App\Orchid\Layouts\LookupListLayout;
 use Orchid\Screen\Actions\Link;
 use Orchid\Screen\Screen;
+use Orchid\Support\Facades\Alert;
 
 class LookupScreen extends Screen
 {
@@ -41,5 +43,12 @@ class LookupScreen extends Screen
         return [
             LookupListLayout::class
         ];
+    }
+
+    public function remove(Request $request): void
+    {
+        ModelsLookup::findOrFail($request->get('id'))->delete();
+
+        Alert::info('You have successfully deleted the lookup.');
     }
 }
