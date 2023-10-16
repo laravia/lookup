17bef90b75b3e71fcc1895f753a8f45f0d924c6a
manuel <code@leunam.art>
2023-10-16 17:52:10 +0200

    [minor] changed typo

--- app/Orchid/Layouts/LookupListLayout.php
+++ app/Orchid/Layouts/LookupListLayout.php
@@ -44,7 +44,7 @@ class LookupListLayout extends Table
 
                         Button::make(__('Delete'))
                             ->icon('bs.trash3')
-                            ->confirm(__('Once the account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.'))
+                            ->confirm(__('Once the lookup entry is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.'))
                             ->method('remove', [
                                 'id' => $lookup->id,
                             ]),
